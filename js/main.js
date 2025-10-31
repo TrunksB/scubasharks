4b5006f6a9ddad694b1b06f48fd76d097da37baa
trunksb <nkwangaozilronie@gmail.com>
2025-10-31 20:18:44 +0300

    Nkwanga Ronald +256703528568 nkwangaozilronie@gmail.com Changed the header menu(Ribbon) from grid element to a nav element with an un ordered list on all the pages Added media queries to all css files to create resposiveness on all pages when displayed on smaller screens Added menu for android which stays hidden on large screens and is opened and closed onclick controlled by functions defined in the javascript file.

---
+++ js/main.js
@@ -0,0 +1,10 @@
+
+
+ function showsidebar(){
+    const openSideBar = document.querySelector(".side_bar");
+    openSideBar.style.display = 'flex';
+}
+function hidesidebar(){
+    const openSideBar = document.querySelector(".side_bar");
+    openSideBar.style.display = 'none';
+}
